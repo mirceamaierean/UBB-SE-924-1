f4aed319e80fe0d7beac96d7d2ad6034b6df751d
Ignat Alex-Matei <ignatalexmatei@yahoo.com>
2024-05-22 13:18:50 +0300

    Added name on chats

---
+++ 924-server/Migrations/20240522100108_AddNameChat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _924_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameChat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Chats",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "New Chat");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Chats");
+        }
+    }
+}
